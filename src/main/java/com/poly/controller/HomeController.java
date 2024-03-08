e650ff14a1f79d2f5eede1f85816b4fa8a6633ef
LuongVanQuoc <113768193+LuongVanQuoc@users.noreply.github.com>
2024-03-08 22:08:16 +0700

    Update Navigation

--- src/main/java/com/poly/controller/HomeController.java
+++ src/main/java/com/poly/controller/HomeController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 	@RequestMapping("")
 	public String home() {
+		return "redirect:/shop/home";
+	}
+	
+	@RequestMapping("shop/home")
+	public String shopHome() {
 		return "shop/index";
 	}
 	
