895dbbb4c9df34fac7647f0c4c47b6d3fd8b4140
Quang đẹp trai nhất Trái Đất <133572908+QuangHotBoy@users.noreply.github.com>
2024-03-08 12:23:13 +0700

    aaaa

---
+++ src/main/java/com/poly/a.java
@@ -0,0 +1,16 @@
+package com.poly;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class a {
+	@RequestMapping("shop/home")
+	public String index() {
+		return "index";
+	}
+	@RequestMapping("shop/a")
+	public String a1() {
+		return "pages/forms/basic_elements";
+	}
+}
