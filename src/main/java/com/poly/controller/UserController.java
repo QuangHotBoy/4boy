ee6c1cea5283b1ab6823e374d559bb060f3acf6d
Võ Hoàng Gia Bảo <huynhngocbaotran17@gmail.com>
2024-03-09 11:24:36 +0700

    giao diện quản lý trang cá nhân

--- src/main/java/com/poly/controller/UserController.java
+++ src/main/java/com/poly/controller/UserController.java
@@ -24,7 +24,36 @@ public class UserController {
         return "shop/layout/user/information-changepass";
     }
 
-    
+    @RequestMapping("/user/address")
+    public String address(Model model) {
+        return "shop/layout/user/address";
+    }
+
+    @RequestMapping("/user/address/add-new-address")
+    public String addnewaddress(Model model) {
+        return "shop/layout/user/add-new-address";
+    }
+
+    @RequestMapping("/user/address/update-address")
+    public String updateaddress(Model model) {
+        return "shop/layout/user/update-address";
+    }
+
+    @RequestMapping("/user/invoice")
+    public String invoice(Model model) {
+        return "shop/layout/user/invoice";
+    }
+
+    @RequestMapping("/user/invoice/detail-invoice")
+    public String detaiinvoice(Model model) {
+        return "shop/layout/user/detail-invoice";
+    }
+
+
+    @RequestMapping("/user/favorite")
+    public String favorite(Model model) {
+        return "shop/layout/user/favorites";
+    }
 
     
 }
