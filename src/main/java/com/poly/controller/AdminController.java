ca59a4bb26e48f4e2dffae608d4c2ac44cbf4898
Quang đẹp trai nhất Trái Đất <133572908+QuangHotBoy@users.noreply.github.com>
2024-03-14 19:11:32 +0700

    magiamgia

--- src/main/java/com/poly/controller/AdminController.java
+++ src/main/java/com/poly/controller/AdminController.java
@@ -1,5 +1,8 @@
 package com.poly.controller;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +10,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -25,15 +33,17 @@ public class AdminController {
 		return "index";
 	}
 	@RequestMapping("shop/admin/voucher")
-	public String voucher(Model model, @RequestParam(defaultValue = "0") int page) {
-		int pageSize = 5; // Số sản phẩm trên mỗi trang
-		Page<MaGiamGia> voucherPage = mggDAO.findAll(PageRequest.of(page, pageSize));
-		model.addAttribute("vouchers", voucherPage.getContent());
-		model.addAttribute("currentPage", page);
-		model.addAttribute("totalPages", voucherPage.getTotalPages());
-		List<MaGiamGia> vouchers = mggDAO.findAll();
-	model.addAttribute("vouchers", vouchers);
+	public String voucher(Model model) {
+		List<MaGiamGia> mggs = mggDAO.findAll();	
+		model.addAttribute("mggs", mggs);
 		return "admin/voucher/voucher";
+	}
+	@RequestMapping("shop/admin/addnew_mgg")
+	public String add_voucher_new(@Validated @ModelAttribute("mggs") MaGiamGia mggs,
+			BindingResult result, Model model) throws IllegalAccessException, IOException {
+			LocalDate currentDate = LocalDate.now();
+			model.addAttribute("ngayBatDau", currentDate);
+			return "admin/voucher/add_voucher";
 	}
 	@RequestMapping("shop/admin/order")
 	public String order() {
