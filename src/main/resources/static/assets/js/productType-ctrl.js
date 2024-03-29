1c46b0a4e0c643ca3c5a4da4d31f79ed95353150
https://github.com/LuongVanQuoc/DATN.git <nguyenhieuct0308@gmail.com>
2024-03-29 15:59:32 +0700

    Hoàn thành loại

--- src/main/resources/static/assets/js/productType-ctrl.js
+++ src/main/resources/static/assets/js/productType-ctrl.js
@@ -6,17 +6,22 @@ app.controller("myCtrl", function ($scope, $http) {
 
     $scope.initialize = function () {
         $http.get("/rest/types").then(resp => {
-            if (Array.isArray(resp.data)) {
-                $scope.types = resp.data;
-            } else {
-                $scope.types = [resp.data];
-            }
+            $scope.types = resp.data;
 
             console.log($scope.types);
+
+            // // Kiểm tra xem DataTable đã được khởi tạo trên bảng chưa
+            // if (!$.fn.dataTable.isDataTable('#table1')) {
+            //     // Nếu chưa được khởi tạo, thì mới áp dụng DataTable
+            //     $('#table1').DataTable();
+            // }
         });
     }
 
     $scope.initialize();
+
+
+   
 
 
     //xoa form
@@ -30,6 +35,7 @@ app.controller("myCtrl", function ($scope, $http) {
             .then(function (response) {
                 // Sao chép dữ liệu từ phản hồi vào biến form để điền vào form chỉnh sửa
                 $scope.form = angular.copy(response.data);
+                $('#exampleModal1').modal('show');
                 console.log(id);
             })
             .catch(function (error) {
@@ -38,10 +44,19 @@ app.controller("myCtrl", function ($scope, $http) {
             });
     };
 
-
+    // $scope.edit = function(item){
+    // 	$scope.form = angular.copy(item);
+    // 	$("exampleModal1").tab("show");
+    // }
 
     //hàm thêm
     $scope.create = function () {
+        // Kiểm tra dữ liệu trống
+        // if (!$scope.form || !$scope.form.name || !$scope.form.description) {
+        //     alert("Vui lòng nhập đầy đủ thông tin sản phẩm!");
+        //     return;
+        // }
+    
         var item = angular.copy($scope.form);
         $http.post('/rest/types', item).then(resp => {
             if (!$scope.items) {
@@ -56,31 +71,32 @@ app.controller("myCtrl", function ($scope, $http) {
             console.log("Error", error);
         });
     }
+    
     //hàm cập nhật
-    $scope.form = {}; // Initialize $scope.form as an empty object
     $scope.update = function () {
-        if (!$scope.form || !$scope.form.id) {
-            alert("Invalid form data. Cannot update product.");
-            return;
-        }
-
         var item = angular.copy($scope.form);
-        $http.put('/rest/productsType/${item.id}', item)
-            .then(resp => {
-                var index = $scope.items.findIndex(p => p.id === item.id);
-                if (index !== -1) {
-                    $scope.items[index].tenLoai = item.tenLoai;
-                    $scope.items[index].moTa = item.moTa;
-                    alert("Cập nhật sản phẩm thành công!");
-                } else {
-                    alert("Không tìm thấy sản phẩm cần cập nhật trong danh sách!");
-                }
-            })
-            .catch(error => {
-                console.log("Error", error);
-                alert("Lỗi cập nhật sản phẩm!");
-            });
-    };
+        $http.put('/rest/types/' + item.id, item).then(resp => {
+            // Kiểm tra nếu $scope.items chưa được khởi tạo, thì khởi tạo nó là một mảng trống
+            if (!$scope.items) {
+                $scope.items = [];
+            }
+
+            // Tìm và cập nhật mục đã được chỉnh sửa trong mảng $scope.items
+            var index = $scope.items.findIndex(i => i.id === item.id);
+            if (index !== -1) {
+                $scope.items[index] = angular.copy(item);
+            }
+
+            $scope.reset();
+            alert("Cập nhật sản phẩm thành công!");
+            loadData();
+        }).catch(error => {
+            alert("Lỗi cập nhật sản phẩm!");
+            console.log("Error", error);
+        });
+    }
+
+
 
 
 
@@ -95,41 +111,9 @@ app.controller("myCtrl", function ($scope, $http) {
         });
     }
 
-    $scope.pager = {
-        page: 0,
-        size: 5,
-        get types() {
-            if (this.page < 0) {
-                this.last();
-            }
-            if (this.page >= this.count) {
-                this.first();
-            }
-            var start = this.page * this.size;
-            return $scope.types.slice(start, start + this.size)
-        },
-        get count() {
-            return Math.ceil(1.0 * $scope.types.length / this.size);
-        },
-        first() {
-            this.page = 0;
-        },
-        last() {
-            this.page = this.count - 1;
-        },
-        next() {
-            this.page++;
-        },
-        prev() {
-            this.page--;
-        }
-    }
 
 
 
 
 
 });
-
-
-
