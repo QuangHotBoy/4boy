33f308c579244b3af009f7a3fafdea6a5f30be98
Quang đẹp trai nhất Trái Đất <133572908+QuangHotBoy@users.noreply.github.com>
2024-04-11 05:32:37 +0700

    aaaaa

--- src/main/resources/static/assets/js/voucher-ctrl.js
+++ src/main/resources/static/assets/js/voucher-ctrl.js
@@ -47,32 +47,126 @@ app.controller("voucher-ctrl", function($scope, $http){
             $scope.form = {};
         });
 };
+function validateForm(){
+    var id = document.getElementById("id").value;
+    var tenMa = document.getElementById("tenMa").value;
+    var soTienGiam = document.getElementById("soTienGiam").value;
+    var soLuong = document.getElementById("soLuong").value;
+    var thongTin = document.getElementById("thongTin").value;
+    var dieuKien = document.getElementById("dieuKien").value;
+    if (id.trim() == "") {
+        iziToast.warning({
+            title: 'Thông báo',
+            message: 'Vui lòng nhập mã giảm giá.',
+            position: 'topRight'
+        });
+        return false;
+    }
+    if (isMaGiamGiaTrung(id)) {
+        iziToast.warning({
+            title: 'Thông báo',
+            message: 'Mã giảm giá đã tồn tại!!',
+            position: 'topRight'
+        });
+        return false;
+    }
+    if (tenMa.trim() == "") {
+        iziToast.warning({
+            title: 'Thông báo',
+            message: 'Vui lòng nhập tên chương trình.',
+            position: 'topRight'
+        });
+        return false;
+    }
+    if (soTienGiam.trim() == "") {
+        iziToast.warning({
+            title: 'Thông báo',
+            message: 'Vui lòng nhập số tiền giảm.',
+            position: 'topRight'
+        });
+        return false;
+    }else if (parseInt(soTienGiam) <= 0) { // Kiểm tra nếu số lượng là không hoặc âm
+        iziToast.warning({
+            title: 'Thông báo',
+            message: 'Số tiền giảm phải lớn hơn 0.',
+            position: 'topRight'
+        });
+        return false;
+    }
+    if (soLuong.trim() == "") {
+        iziToast.warning({
+            title: 'Thông báo',
+            message: 'Vui lòng nhập số lượng.',
+            position: 'topRight'
+        });
+        return false;
+    }else if (parseInt(soLuong) <= 0) { // Kiểm tra nếu số lượng là không hoặc âm
+        iziToast.warning({
+            title: 'Thông báo',
+            message: 'Số lượng phải lớn hơn 0.',
+            position: 'topRight'
+        });
+        return false;
+    }
+    if (thongTin.trim() == "") {
+        iziToast.warning({
+            title: 'Thông báo',
+            message: 'Vui lòng nhập mô tả.',
+            position: 'topRight'
+        });
+        return false;
+    }
+    if (dieuKien.trim() == "") {
+        iziToast.warning({
+            title: 'Thông báo',
+            message: 'Vui lòng nhập đơn tối thiểu.',
+            position: 'topRight'
+        });
+        return false;
+    }else if (parseInt(dieuKien) <= 0) { // Kiểm tra nếu số lượng là không hoặc âm
+        iziToast.warning({
+            title: 'Thông báo',
+            message: 'Đơn tối thiểu phải lớn hơn 0.',
+            position: 'topRight'
+        });
+        return false;
+    }
+    if (!$scope.form.ngayKetThuc) {
+        alert("Vui lòng chọn ngày kết thúc!");
+        return false;
+    }
+    if (new Date($scope.form.ngayBatDau) >= new Date($scope.form.ngayKetThuc)) {
+        alert("Ngày bắt đầu phải trước ngày kết thúc!");
+        return false;
+    }
+    // if ($scope.form.soLuong <= 0 || $scope.form.dieuKien <= 0 || $scope.form.soTienGiam <= 0) {
+    //     alert("Số lượng phải là số dương!");
+    //     return false;
+    // }
+    // var regex = /^\d+$/;
+    // if (!regex.test($scope.form.soLuong) || !regex.test($scope.form.dieuKien) || !regex.test($scope.form.soTienGiam)) {
+    //     alert("Vui lòng nhập số!");
+    //     return false;
+    // }
+    return true;
+}
+function isMaGiamGiaTrung(id) {
+    // Lặp qua danh sách các loại mã giảm giá đã tải từ server
+    for (var i = 0; i < $scope.vouchers.length; i++) {
+        if ($scope.vouchers[i].id === id) {
+            return true; // Nếu trùng, trả về true
+        }
+    }
+    return false; // Nếu không trùng, trả về false
+}
+
+
+
     //them mgg moi
     $scope.create = function() {
         // Kiểm tra xem đã tồn tại mã giảm giá có cùng ID chưa
-        var duplicateId = $scope.vouchers.some(function(voucher) {
-            return voucher.id === $scope.form.id;
-        });
-        if (!$scope.form.id || !$scope.form.tenMa || !$scope.form.soTienGiam || !$scope.form.soLuong || !$scope.form.ngayBatDau || !$scope.form.ngayKetThuc || !$scope.form.thongTin) {
-            alert("Vui lòng điền đầy đủ thông tin vào các trường bắt buộc!");
-            return;
-        }
-        // Nếu mã giảm giá có ID trùng lặp, hiển thị thông báo lỗi và không thực hiện thêm mới
-        if (duplicateId) {
-            alert("Mã giảm giá đã tồn tại. Vui lòng chọn mã khác.");
-            return;
-        }
-        if (new Date($scope.form.ngayBatDau) >= new Date($scope.form.ngayKetThuc)) {
-            alert("Ngày bắt đầu phải trước ngày kết thúc!");
-            return;
-        }
-        if ($scope.form.soLuong <= 0 || $scope.form.dieuKien <= 0 || $scope.form.soTienGiam <= 0) {
-            alert("Số lượng phải là số dương!");
-            return;
-        }
-        var regex = /^\d+$/;
-        if (!regex.test($scope.form.soLuong) || !regex.test($scope.form.dieuKien) || !regex.test($scope.form.soTienGiam)) {
-            alert("Vui lòng nhập số!");
+        if (!validateForm()) {
+            // Nếu form không hợp lệ, không tiến hành thêm mới
             return;
         }
         
@@ -93,16 +187,25 @@ app.controller("voucher-ctrl", function($scope, $http){
                     $scope.vouchers = []; // Initialize vouchers array if it doesn't exist
                 }
                 // Prepend the new data to the vouchers array
-                $scope.vouchers.unshift(resp.data);
+               
                 $scope.form = {}; // Clear form data
-                alert("Thêm mới sản phẩm thành công!");
-                // Optionally, load data again if needed
-                // loadData();
+                $scope.vouchers.push(resp.data);
+                $scope.reset();
+                iziToast.info({
+                    title: 'Thông báo',
+                    message: 'Thêm mới mã giảm giá thành công!',
+                    position: 'topRight'
+                });
             })
-            .catch(error => {
-                alert("Lỗi thêm mới sản phẩm!");
+            .catch(function(error) {
+                // Hiển thị thông báo lỗi bằng thư viện iziToast
+                iziToast.error({
+                    title: 'Lỗi',
+                    message: 'Cập nhật sản phẩm không thành công: ' + error.statusText,
+                    position: 'topRight'
+                });
                 console.log("Error", error);
-            });
+            }); 
     };
     
     
@@ -147,10 +250,15 @@ app.controller("voucher-ctrl", function($scope, $http){
     
                 // Đặt lại form và hiển thị thông báo cập nhật thành công
                 $scope.reset();
-                alert("Cập nhật sản phẩm thành công!");
-    
-                // Optional: load lại dữ liệu nếu cần
-                // $scope.initialize();
+                iziToast.info({
+                    title: 'Thông báo',
+                    message: 'Cập nhật mã giảm giá thành công!',
+                    position: 'topRight'
+                });
+                // Chờ 3 giây trước khi thực hiện reload
+                setTimeout(function () {
+                    location.reload();
+                }, 1000);
             })
             .catch(function(error) {
                 // Hiển thị thông báo lỗi nếu cập nhật không thành công
